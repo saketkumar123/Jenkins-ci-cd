72e32c998b903087bbd63a34bc10fd7a51ccbf46
saketkumar123 <saket.ulsi@gmail.com>
2023-06-18 16:00:31 +0530

    Commit first springboot project

---
+++ jenkins-ci-cd/src/test/java/com/cicd/lab/jenkinscicd/JenkinsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.lab.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
